7b05850506acc25bd7cccecf703358720da6a8a4
Simon Tirant <simon.tirant@gmail.com>
2021-12-09 20:30:08 +0100

    minor changes to Lazy loading

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './index.css';
 
 // Components imports
+import Header from './Components/Layout/Header';
+import Footer from './Components/Layout/Footer';
 const Home = lazy(() => import('./Components/Home'));
 const Projects = lazy(() => import("./Components/Projects"));
 const About = lazy(() => import("./Components/About"));
 const Contact = lazy(() => import("./Components/Contact"));
-const Header = lazy(() => import('./Components/Layout/Header'));
-const Footer = lazy(() => import('./Components/Layout/Footer'));
 const Error = lazy(() => import('./Components/Error'));
 
 ReactDOM.render(
