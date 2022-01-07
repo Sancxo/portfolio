3463d2eaf20e9fc6d5e5ec3bdd623e88226dd264
Simon Tirant <simon.tirant@gmail.com>
2022-01-07 20:09:17 +0100

    starting projects page

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -1,6 +1,8 @@
 function Projects() {
     return (
-        <h1>My Projects</h1>
+        <div style={{marginTop: "3em", padding: "0 12.5%"}}>
+            <h3>My Projects</h3>
+        </div>
     );
 };
 
