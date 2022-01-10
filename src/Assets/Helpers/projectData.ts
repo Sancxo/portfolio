656ebb9ceb17e3920aff8e5d66c6c67de7116e27
Simon <simon@7lieues.io>
2022-01-10 12:20:01 +0100

    adding get-gifs project

--- src/Assets/Helpers/projectData.ts
+++ src/Assets/Helpers/projectData.ts
@@ -40,7 +40,8 @@ const projectList: {
         category: "Training course",
         desc: 'Dynamic front-end e-commerce web page made in React for a course on OpenClassrooms.', // Features: dynamic cart (open/close, add items), automatic item catalog with parameters from model, light/dark mode switch written in localStorage, type filter.
         technos: ["JavaScript", "React"]
-    },    {
+    },    
+    {
         id: "wio",
         name: "Watch It Outside",
         url: "https://sancxo.github.io/watch-it-outside",
@@ -49,6 +50,15 @@ const projectList: {
         desc: "Responsive website for a fictionnal film festival in Boston made as an exam for my full-stack training class at OpenClassrooms.",
         technos: ["JavaScript", "HTML", "CSS", "Bootstrap"]
     },
+    {
+        id: "get-gifs",
+        name: "Get Gifs",
+        url: "http://itlolcu.cluster031.hosting.ovh.net/get-gifs",
+        cover: "",
+        category: "Freelance and tests",
+        desc: "Technical test for a former employer where I should make a React app which connect to Giphy Api to display the platform's Top 20 gifs, a random gif feature, a functional search bar with infinite scroll and a dark mode",
+        technos: ["JavaScript", "React", "Bootstrap"]
+    }
 ]
 
 const techIcons: {[index: string]: string} = {
