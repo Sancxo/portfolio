8edbc24f731110a6c632de9ef2e9d62f4f45ac2b
Simon Tirant <simon.tirant@gmail.com>
2022-01-21 15:26:36 +0100

    Starting contact form frontend

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -1,6 +1,27 @@
+import styled from "styled-components";
+
+const ContactForm = styled.form`
+    display: flex;
+    flex-direction: column;
+`
 function Contact() {
     return (
-        <h1>Contact Me</h1>
+        <div>
+            <h2>Contact Me</h2>
+
+            <ContactForm action="#">
+                <label htmlFor="name">Name(*)</label>
+                <input type="text" name="Name:" id="name" required/>
+                <label htmlFor="fName">First name</label>
+                <input type="text" name="First name" id="fName" />
+                <label htmlFor="email">E-mail(*)</label>
+                <input type="email" name="email" id="email" required />
+                <label htmlFor="phone">Phone</label>
+                <input type="tel" name="phone" id="phone" />
+                <label htmlFor="message">Your message(*)</label>
+                <textarea name="message" id="message" cols={30} rows={10} required></textarea>
+            </ContactForm>
+        </div>
     )
 }
 
